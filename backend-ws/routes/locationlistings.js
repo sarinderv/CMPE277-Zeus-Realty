fcc57cee363be905ba77764dbb3288c03c99d61a
Varun Alla <varun.alla@sjsu.edu>
2022-05-08 19:56:06 -0700

    updated the location service

--- backend-ws/routes/locationlistings.js
+++ backend-ws/routes/locationlistings.js
@@ -3,15 +3,28 @@ const router = express.Router();
 const locationlistings = require('../services/locationlistings');
 
 /* GET locationlistings. */
-router.get('/', async function(req, res, next) {
+router.get('/point', async function(req, res, next) {
   try {
-    res.json(await locationlistings.findLocationbyCenter(1,1));
+    let query=req.query;
+    console.log(query.lat);
+    console.log(query.long);
+    res.json(await locationlistings.findLocationbyCenter(parseFloat(query.lat),parseFloat(query.long)));
   } catch (err) {
     console.error(`Error while getting listings `, err.message);
     res.status(err.statusCode || 500).json({'message': err.message});
   }
 });
-
+router.get('/bounds', async function(req, res, next) {
+  try {
+    let query=req.query;
+    console.log(query.lat);
+    console.log(query.long);
+    res.json(await locationlistings.findLocationbyCenter(parseFloat(query.lat),parseFloat(query.long)));
+  } catch (err) {
+    console.error(`Error while getting listings `, err.message);
+    res.status(err.statusCode || 500).json({'message': err.message});
+  }
+});
 
 
 module.exports = router;
--- backend-ws/services/locationlistings.js
+++ backend-ws/services/locationlistings.js
@@ -2,8 +2,21 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
-function findLocationbyCenter(lat, lang) {
-    const query = { "location": { $near: { $geometry: { type: "Point", coordinates: [-121.89, 37.2] }, $minDistance: 10000, $maxDistance: 50000 } } };
+function findLocationbyCenter(lat, long) {
+    //-121.89, 37.2
+    const query = { "location": { $near: { $geometry: { type: "Point", coordinates: [long,lat] }, $minDistance: 10000, $maxDistance: 50000 } } };
+console.log(JSON.stringify(query));
+    return new Promise((resolve, reject) => {
+        db.client.db("sample_geospatial").collection("propertylistings").find(query).toArray(function (err, result) {
+            if (err) throw reject(err);
+            resolve(result);
+
+        });
+    })
+}
+function findLocationbyBounds(lat, long) {
+    //-121.89, 37.2
+    const query = { "location": { $near: { $geometry: { type: "Point", coordinates: [long,lat] }, $minDistance: 10000, $maxDistance: 50000 } } };
 
     return new Promise((resolve, reject) => {
         db.client.db("sample_geospatial").collection("propertylistings").find(query).toArray(function (err, result) {
@@ -12,7 +25,6 @@ function findLocationbyCenter(lat, lang) {
 
         });
     })
-
 }
 module.exports = {
     findLocationbyCenter
