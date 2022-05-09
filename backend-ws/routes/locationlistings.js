9a0c8830abe8ef3a210f4c7e9b0e084c94f20016
Varun Alla <varun.alla@sjsu.edu>
2022-05-08 20:06:44 -0700

    added bounds api to fetch locations within a window

--- backend-ws/routes/locationlistings.js
+++ backend-ws/routes/locationlistings.js
@@ -16,10 +16,11 @@ router.get('/point', async function(req, res, next) {
 });
 router.get('/bounds', async function(req, res, next) {
   try {
-    let query=req.query;
-    console.log(query.lat);
-    console.log(query.long);
-    res.json(await locationlistings.findLocationbyCenter(parseFloat(query.lat),parseFloat(query.long)));
+    let bounds=req.query.bounds;
+    bounds=bounds.split(',');
+    bounds=bounds.map(e=>parseFloat(e));
+    console.log(bounds);
+    res.json(await locationlistings.findLocationbyBounds(bounds));
   } catch (err) {
     console.error(`Error while getting listings `, err.message);
     res.status(err.statusCode || 500).json({'message': err.message});
--- backend-ws/services/locationlistings.js
+++ backend-ws/services/locationlistings.js
@@ -4,8 +4,8 @@ const config = require('../config');
 
 function findLocationbyCenter(lat, long) {
     //-121.89, 37.2
-    const query = { "location": { $near: { $geometry: { type: "Point", coordinates: [long,lat] }, $minDistance: 10000, $maxDistance: 50000 } } };
-console.log(JSON.stringify(query));
+    const query = { "location": { $near: { $geometry: { type: "Point", coordinates: [long, lat] }, $minDistance: 10000, $maxDistance: 50000 } } };
+    console.log(JSON.stringify(query));
     return new Promise((resolve, reject) => {
         db.client.db("sample_geospatial").collection("propertylistings").find(query).toArray(function (err, result) {
             if (err) throw reject(err);
@@ -14,9 +14,15 @@ console.log(JSON.stringify(query));
         });
     })
 }
-function findLocationbyBounds(lat, long) {
+function findLocationbyBounds(box) {
     //-121.89, 37.2
-    const query = { "location": { $near: { $geometry: { type: "Point", coordinates: [long,lat] }, $minDistance: 10000, $maxDistance: 50000 } } };
+    //  loc: { $geoWithin: { $box:  [ [ 0, 0 ], [ 100, 100 ] ] } }
+    //, 
+    //, 
+    //  box=[ [ -122.17444444788339, 37.20444446317234 ], [ -121.80245270136577, 37.50622517874518 ] ] ;
+    
+    let boxes = [[box[0], box[1]], [box[2], box[3]]];
+    const query = { "location": { $geoWithin: { $box: boxes } } };
 
     return new Promise((resolve, reject) => {
         db.client.db("sample_geospatial").collection("propertylistings").find(query).toArray(function (err, result) {
@@ -27,5 +33,6 @@ function findLocationbyBounds(lat, long) {
     })
 }
 module.exports = {
-    findLocationbyCenter
+    findLocationbyCenter,
+    findLocationbyBounds
 }
