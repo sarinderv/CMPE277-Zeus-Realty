88343637a05ed9b30bfc3384f91a8c2599fc2350
Varun Alla <varun.alla@sjsu.edu>
2022-05-11 13:33:27 -0700

    added api for mongodb

--- android-app/app/src/main/java/com/cmpe277/project/zeusrealty/apiservice/IListingAPI.java
+++ android-app/app/src/main/java/com/cmpe277/project/zeusrealty/apiservice/IListingAPI.java
@@ -9,12 +9,17 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.Path;
 
 public interface IListingAPI {
         @GET("/locations/point")
         Call<List<LocationAPIResponse>> getLocationDetailsatCenter(@Query("lat") String lat,@Query("long") String longitude);
         @GET("/locations/bounds")
         Call<List<LocationAPIResponse>> getLocationDetailsInsideRectangle(@Query("bounds") String bounds);
+        @GET("/listings/byproperty/{id}")
+        Call<PropertyListingAPIResponseContainer> getLocationDetailsById(@Path(value="id") String propertyid);
+
         @GET("/listings")
         Call<PropertyListingAPIResponseContainer> getPropertyListings();
+
 }
