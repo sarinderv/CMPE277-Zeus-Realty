5d74ceffa26e843e179a0ecfe98b5be88a0547e6
hasinireddy23 <hasinireddy765@gmail.com>
2022-05-11 11:13:16 -0700

    added details page along with navigation

---
+++ android-app/app/src/main/java/com/cmpe277/project/zeusrealty/TestFragment.java
@@ -0,0 +1,21 @@
+package com.cmpe277.project.zeusrealty;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TestFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView= inflater.inflate(R.layout.fragment_test, container, false);
+        return rootView;
+    }
+}
